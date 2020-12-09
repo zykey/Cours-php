58fbb966031382af2edeff2aa9acb6597ca63dae
Samy <samy.latifi18@gmail.com>
2020-12-09 11:47:43 +0100

    fin exo 1

---
+++ exo 1/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Première page !</h1>
+    <?php 
+    
+    echo '<ul><li>pomme</li><li>tomate</li><li>poire</li><li>orange</li><li>raisin</li></ul>'
+    
+    ?>
+</body>
+</html>
