c2d368a36f0f0d00a85d798a74aada80cbf792e0
Samy <samy.latifi18@gmail.com>
2020-12-11 20:02:32 +0100

    exo 10

---
+++ exo 10/index.php
@@ -0,0 +1,8 @@
+<?php 
+//Exercice 10-c :
+
+    //Avec la fonction date(), afficher a l'ecran la date qu'il sera dans 26 jours et 16h sous le format suivant :
+    //2020-12-11 06:24:30
+
+    
+?>
