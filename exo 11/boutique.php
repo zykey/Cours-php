a7a1549231ea657afb3fd0bf789b209bf85f5a68
Samy <samy.latifi18@gmail.com>
2020-12-14 11:05:28 +0100

    Exo 11

---
+++ exo 11/boutique.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <ul>
+        <li><a href="./accueil.php">Accueil</a></li>
+        <li><a href="./contact.php">Contact</a></li>
+        <li><a href="./boutique.php">Boutique</a></li>
+    </ul>
+    <h1>Site 3</h1>
+</body>
+</html>
